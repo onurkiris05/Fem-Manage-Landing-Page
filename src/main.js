55bd8d0a950f47132ee16fc8a1d06d6eb4bf8bef
onurkiris <83399747+onurkiris05@users.noreply.github.com>
2025-01-28 23:27:12 +0300

    [Updated] Nav and Background

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 const primaryHeader = document.querySelector(".primary-header");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
+const hamburgerIcon = document.querySelector(".icon-hamburger");
+const closeIcon = document.querySelector(".icon-close");
 
 navToggle.addEventListener("click", () => {
   const isVisible = primaryNav.toggleAttribute("data-visible");
   navToggle.setAttribute("aria-expanded", isVisible);
   primaryHeader.toggleAttribute("data-overlay", isVisible);
+
+  hamburgerIcon.style.display = isVisible ? "none" : "block";
+  closeIcon.style.display = isVisible ? "block" : "none";
 });
 
 const slider = new A11YSlider(document.querySelector(".slider"), {
